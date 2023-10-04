6697feb7c5963bbbd98f022c0d6fa4a7208d7a2c
Богдан Харченко <bohdan.kharchenko@publicfinance.gov.ua>
2023-10-04 17:31:27 +0300

    Add project files.

---
+++ Homework_7/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Nodes;
+
+namespace Homework_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var checkUserChoise = true;
+
+            do
+            {
+                Console.WriteLine($"Оберіть тип дії: \n\t 1 - Переглянути історію витрат \n\t 2 - Додати нову витрату \n\t 3 - Вивести статистику");
+                string userChoise = Console.ReadLine();
+
+                if(userChoise == "1" || userChoise == "2" || userChoise == "3")
+                {
+                    checkUserChoise = false;
+
+                    switch(Convert.ToInt32(userChoise))
+                    {
+                        case 1:
+                            ShowCostsHistory();
+                            break;
+
+                        case 2:
+                            AddCost();
+                            break;
+
+                        case 3:
+                            ShowStatistics();
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Недопустиме значення.");
+                }
+            }
+            while (checkUserChoise);
+        }
+
+        private static void ShowCostsHistory()
+        {
+            using (FileStream fs = new FileStream("Costs.json", FileMode.OpenOrCreate))
+            {
+                Console.WriteLine(51);
+                JsonSerializer jsonreader = new JsonSerializer();
+                //jsonreader.Deserialize<Cost>
+
+            }
+        }
+        private static void AddCost()
+        {
+            
+        }
+        private static void ShowStatistics()
+        {
+            
+        }
+    }
+
+    public class Cost
+    {
+        decimal sum;
+        DateTime date;
+        string type;
+    }
+}
